cc95fc1406fb2b5afec5696138be9e554eade598
Bruno Wollmann <bwollmann@saskpower.com>
2020-04-15 15:07:03 -0600

    Added some error checking

--- CiscoASA/anyconnect.py
+++ CiscoASA/anyconnect.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python
 
-import sys
 from netmiko import ConnectHandler
 from getpass import getpass
 from datetime import datetime
 from openpyxl import Workbook, load_workbook
 from openpyxl.utils.cell import get_column_letter
-from openpyxl.worksheet.table import Table, TableStyleInfo
+from openpyxl.worksheet.table import Table
 
 
 def main():
-
-    # print('\n')
-    # username = input('Username:')
-    # password = getpass()
-    # print('\n')
 
     username, password = get_creds()
 
@@ -34,10 +28,9 @@ def main():
 
     now = datetime.now()
     tab_name = now.strftime('%Y_%m_%d_%H_%M_%S')
-    results = []
 
+    results = []
     for firewall in [primary, secondary]:
-        print('Gathering information from {}'.format(firewall['host']))
         results.append(firewall['host'])
         vpn_sessiondb = show_vpn_sessiondb(firewall)
         results.append(vpn_sessiondb)
@@ -49,14 +42,15 @@ def get_creds():
     '''
     '''
 
-    print('\n')
     print('-' * 40)
-    un = input('Username, (q) to quit:')
+
+    un = input('Username, (q) to quit: ')
     if un.lower() == 'q': exit('EXITING')
     pw = getpass()
     if pw.lower() == 'q': exit('EXITING')
+
     print('-' * 40)
-    print('\n')
+
     return(un, pw)
 
 
@@ -64,7 +58,19 @@ def show_vpn_sessiondb(device):
     '''
     '''
 
-    net_connect = ConnectHandler(**device)
+    done = False
+    while not done:
+        try:
+            net_connect = ConnectHandler(**device)
+            print('Gathering information from {}'.format(device['host']))
+            done = True
+        except:
+            print('\n')
+            print('ERROR: Invalid username or password for {}'.format(device['host']))
+            username, password = get_creds()
+            device['username'] = username
+            device['password'] = password
+
     output = net_connect.send_command('show vpn-sessiondb anyconnect', use_textfsm=True)
     net_connect.disconnect()
     return(output)
@@ -78,10 +84,8 @@ def output_to_excel(tab, data):
     FILE = r'\AnyConnect.xlsx'
     excel_file = PATH + FILE
 
-    print('Recording in spreadsheet {}'.format(excel_file))
-
     try:
-        wb = load_workbook(filename = excel_file)
+        wb = load_workbook(filename=excel_file)
     except:
         wb = Workbook()
         ws = wb.active
@@ -104,7 +108,6 @@ def output_to_excel(tab, data):
         column_number[heading] = element
 
     row_number = 1
-
     # Iterating through the list
     for item in data:
         if type(item) == str:
@@ -113,18 +116,20 @@ def output_to_excel(tab, data):
             # Iterating through the list of dictionaries
             for row_data in item:
                 row_number += 1
+                # Populating the first cell in the row with the firewall name
                 ws.cell(row_number, 1, hostname)
                 for column_heading in headings:
+                    # Populating the rest of the cells in the row by using
+                    # a dictionary lookup where the column header is the key
                     ws.cell(row_number, column_number.get(column_heading), row_data.get(column_heading))
 
     table_ref = 'A1:{}{}'.format(max_column, row_number)
     table_name = '_{}'.format(tab)
-    vpn_table = Table(displayName = table_name, ref = table_ref)
+    vpn_table = Table(displayName=table_name, ref=table_ref)
     ws.add_table(vpn_table)
     ws.freeze_panes = 'D2'
     wb.save(excel_file)
-    print('There are {} rows and {} columns in the spreadsheet'.format(row_number, number_of_columns))
-    print('The table reference is {}'.format(table_ref))
+    print('Recorded {} rows in spreadsheet {}'.format(row_number, excel_file))
 
 
 if __name__ == "__main__":
